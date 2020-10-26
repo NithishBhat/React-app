8267990782f2bbb44337913afdb4f72a0f1b9a84
nithish bhat <nithish944bhat@gmail.com>
2020-10-26 18:08:56 +0530

    3rd commit uplifting state and using props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import logo from './logo.svg';
 import React, { Component } from 'react';
 import './App.css';
+import { DISHES } from './shared/dishes';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
   render() {
     return (
       <div className="App">
@@ -13,7 +20,7 @@ class App extends Component {
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
-        <Menu />
+        <Menu dishes={this.state.dishes} />
       </div>
     );
   }
