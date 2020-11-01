3823b1688979d1fc722c2d6934ec53c41b31aaa6
nithish bhat <nithish944bhat@gmail.com>
2020-11-01 14:24:06 +0530

    seperarting coontainer and functional components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -57,12 +57,14 @@ class Dishdetail extends Component {
     render() {
         
         return (
+          <div className="container">
                 <div className="row">
                 <div  className="col-12 col-md-5 m-1">
-                {this.renderDish(this.props.dishselect)}
+                {this.renderDish(this.props.dish)}
                 </div>
                <div  className="col-12 col-md-5 m-1">
-               {this.renderDishComment(this.props.dishselect)}
+               {this.renderDishComment(this.props.dish)}
+               </div>
                </div>
                </div>
         );
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,32 +1,20 @@
 import React, { Component } from 'react';
 import { Media } from 'reactstrap';
-
 import { Card, CardImg, CardImgOverlay, CardText, CardBody,
   CardTitle } from 'reactstrap';
-import Dishdetail from './DishdetailComponent';
+
 
 class Menu extends Component {
 
   constructor(props) {
       super(props);
-
-      this.state = {
-          selectedDish: null
-      }
   }
-
-  onDishSelect(dish) {
-      this.setState({ selectedDish: dish});
-  }
-
-
 
   render() {
       const menu = this.props.dishes.map((dish) => {
           return (
-            <div  className="col-12 col-md-5 m-1">
-              <Card key={dish.id}
-                onClick={() => this.onDishSelect(dish)}>
+            <div key={dish.id}  className="col-12 col-md-5 m-1">
+              <Card onClick={()=>this.props.onClick(dish.id)}>
                 <CardImg width="100%" src={dish.image} alt={dish.name} />
                 <CardImgOverlay>
                     <CardTitle>{dish.name}</CardTitle>
@@ -36,14 +24,16 @@ class Menu extends Component {
           );
       });
 
-      return (
-          <div className="container">
-              <div className="row">
-                  {menu}
-              </div>
-              <Dishdetail dishselect={this.state.selectedDish}/>
-          </div>
-      );
+     return (
+
+      <div className="container">
+        <div className="row">
+        {menu}
+        </div>
+        </div>
+     )
+
+
   }
 }
 
